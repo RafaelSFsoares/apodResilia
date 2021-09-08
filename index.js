bf4aa20a906496ff7714a14766bb90e2b3f393ee
Rafael <rafaelsfsoares@gmail.com>
2021-09-08 13:39:56 -0300

    JSfinalizado

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
 
-// $('#procurar').click(function(events) {
-//     events.preventDefault();
-//     nasaApi();
+$('#procurar').click(function(events) {
+    events.preventDefault();
+    nasaApi();
     
-// })
-
-
-
-
-let botaoProcurar = document.querySelector('#procurar')
-
-botaoProcurar.addEventListener('click', ()=> {
-    console.log('Apertei o botão')
-    nasaApi()
-} 
-)
-
-
-
-
+})
 
 
 async function nasaApi (){
+    
     let chave = 'CmJdFBuZnwV1kld7tUPsqYH8uerLBeOxghDyO8iz'
-    let response = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${chave}`)
+    let pesquisar = $('#pesquisar').val();
+    let response = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${chave}&date=${pesquisar}`);
     console.log(response)
 
     let data = await response.json()
@@ -32,8 +19,33 @@ async function nasaApi (){
 }
 
 function apiData(data) {
-    document.querySelector('#container').innerHTML += data.explanation
-    document.querySelector('#container').innerHTML += `<img src = "${data.url}">`
+        
+    $('body').css("background-repeat", "no-repeat");
+    $('body').css("background-size", "cover");
+    $('body').css('background-image', `url(${data.url})`);
+    
+    
+
+    document.querySelector('#texto').innerHTML += data.explanation
+    
 
     
 }
+
+// $('button').on('click', async function (event) {
+//     event.preventDefault();
+//     $('#inicio').css("display", "none");
+//     let valueData = $('#date').val();
+//     let url = `https://api.nasa.gov/planetary/apod?api_key=QRaDd5HWwVtjNo83M6DhwSdhxw9M2Ibxj798RL5w&date=${valueData}`;
+//     let api = await fetch(url).then(response => response.json());
+//     $('body').css("background-image", `url(${api.url})`);
+//     $('body').css("background-repeat", "no-repeat");
+//     $('body').css("background-size", "cover");
+//     $('#corpo').css('display', 'block');
+//     $('#titulo').html(`${api.title}`);
+//     $('#titulo').html(`${api.title}`);
+//     $('#texto').html(`(${api.explanation})`);
+//     console.log(event)
+//     console.log(apiResponse)
+//     console.log(inputDate)
+// });
