a4ffc37e9191a85461cddfd24409ea5af32f59e9
Rafael <rafaelsfsoares@gmail.com>
2021-09-09 17:19:06 -0300

    últimos detalhes: js/css

--- index.js
+++ index.js
@@ -32,20 +32,4 @@ function apiData(data) {
     
 }
 
-// $('button').on('click', async function (event) {
-//     event.preventDefault();
-//     $('#inicio').css("display", "none");
-//     let valueData = $('#date').val();
-//     let url = `https://api.nasa.gov/planetary/apod?api_key=QRaDd5HWwVtjNo83M6DhwSdhxw9M2Ibxj798RL5w&date=${valueData}`;
-//     let api = await fetch(url).then(response => response.json());
-//     $('body').css("background-image", `url(${api.url})`);
-//     $('body').css("background-repeat", "no-repeat");
-//     $('body').css("background-size", "cover");
-//     $('#corpo').css('display', 'block');
-//     $('#titulo').html(`${api.title}`);
-//     $('#titulo').html(`${api.title}`);
-//     $('#texto').html(`(${api.explanation})`);
-//     console.log(event)
-//     console.log(apiResponse)
-//     console.log(inputDate)
-// });
+// melhorar código
